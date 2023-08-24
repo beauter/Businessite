48514e23e4a2c47b0adfb7184a6c27c9608f86e2
Beauter Maraji <106946799+beauter@users.noreply.github.com>
2023-08-24 14:30:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
-// Sample product data
-const products = [
-    { name: "Product 1", description: "Description 1", price: 10 },
-    { name: "Product 2", description: "Description 2", price: 20 },
-    // Add more products here
-];
-
-// Function to display products
-function displayProducts() {
-    const productsSection = document.getElementById("products");
-    productsSection.innerHTML = "";
-    products.forEach(product => {
-        const productDiv = document.createElement("div");
-        productDiv.innerHTML = `
-            <h3>${product.name}</h3>
-            <p>${product.description}</p>
-            <p>Price: $${product.price}</p>
-        `;
-        productsSection.appendChild(productDiv);
-    });
-}
+// Existing JavaScript code
 
 // Submit form function
-document.getElementById("enquiry-form").addEventListener("submit", function(event) {
+document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault();
     const name = document.getElementById("name").value;
     const contact = document.getElementById("contact").value;
     const message = document.getElementById("message").value;
 
     // Send email to the owner
-    const subject = "New Enquiry from Businessite";
+    const subject = "New Enquiry from YRN TECH KENYA";
     const body = `Name: ${name}\nContact: ${contact}\nMessage: ${message}`;
     const emailLink = `mailto:officialbeauter@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
     window.location.href = emailLink;
-});
 
-// Display products when the page loads
-displayProducts();
+    // Show confirmation message
+    const confirmationMessage = document.getElementById("confirmation-message");
+    confirmationMessage.style.display = "block";
+});
